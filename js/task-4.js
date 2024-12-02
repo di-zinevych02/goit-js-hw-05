cecade0bb21b00be6260e9dc97f9bd800cc79428
Diana Zinevych <dianazinevych02@gmail.com>
2024-12-02 22:32:44 +0200

    done fourth task

--- js/task-4.js
+++ js/task-4.js
@@ -36,13 +36,12 @@ const clients = [
   },
 ];
 const getTotalBalanceByGender = (users, gender) =>
-    users
-    .filter(user => user.friends.includes(gender))
-.reduce((total, user)) => {
-    return total + user.balance;
-}
+  users
+    .filter(user => user.gender === gender)
+    .reduce((total, user) => {
+      return total + user.balance;
+    }, 0);
 
 console.log(getTotalBalanceByGender(clients, 'male')); // 12053
 
 console.log(getTotalBalanceByGender(clients, 'female')); // 8863
-
